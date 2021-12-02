971e45a923fe203c78689207390f6b92e7e0de45
涂瑜 <861506831@qq.com>
2021-12-02 23:38:52 +0800

    理解迭代器

--- design-pattern/src/main/java/com/max/design/behavioral/iterator/group/Employee.java
+++ design-pattern/src/main/java/com/max/design/behavioral/iterator/group/Employee.java
@@ -1,5 +1,6 @@
 package com.max.design.behavioral.iterator.group;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
@@ -9,6 +10,7 @@ import lombok.Data;
  * @date 2021-12-02 21:23
  */
 @Data
+@AllArgsConstructor
 public class Employee {
     private String uId;
     private String name;
