a387b9cb666576c05c693559d7cbdd06e9fc7a0c
Rick <861506831@qq.com>
2022-01-20 21:08:21 +0800

    测试提交

--- src/main/java/com/max/learning/entity/TestDTO.java
+++ src/main/java/com/max/learning/entity/TestDTO.java
@@ -1,5 +1,6 @@
 package com.max.learning.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.max.learning.enums.ResultEnum;
 import lombok.Data;
@@ -10,11 +11,68 @@ import java.util.Date;
  * @author Rick
  * @date 2021-11-28 13:18
  */
-@Data
 public class TestDTO {
+
+    @JSONField(name = "name")
+    private String name;
 
     @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss",timezone = "GMT+8")
     private Date time;
 
     private ResultEnum resultEnum;
+
+    private String age;
+
+    private Integer node;
+
+    @JSONField(name = "longStringName")
+    private String longStringName;
+
+    public String getLongStringName() {
+        return longStringName;
+    }
+
+    @JSONField(name = "longStringName")
+    public void setLongStringName(String longStringName) {
+        this.longStringName = longStringName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public ResultEnum getResultEnum() {
+        return resultEnum;
+    }
+
+    public void setResultEnum(ResultEnum resultEnum) {
+        this.resultEnum = resultEnum;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Integer getNode() {
+        return node;
+    }
+
+    public void setNode(Integer node) {
+        this.node = node;
+    }
 }
