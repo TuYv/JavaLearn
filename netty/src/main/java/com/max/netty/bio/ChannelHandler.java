103e9d764297aba0901de26ea1e7142eaee65f7c
涂瑜 <861506831@qq.com>
2022-09-15 21:21:42 +0800

    BIO学习

---
+++ netty/src/main/java/com/max/netty/bio/ChannelHandler.java
@@ -0,0 +1,39 @@
+package com.max.netty.bio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * @author Max.Tu
+ * @program learning
+ * @description
+ * @create 2022-09-15 20:03
+ **/
+public class ChannelHandler {
+
+    private Socket socket;
+    private Charset charset;
+
+    public ChannelHandler(Socket socket, Charset charset) {
+        this.socket = socket;
+        this.charset = charset;
+    }
+
+    public void writeAndFlush(Object msg) {
+        OutputStream out = null;
+        try {
+            out = socket.getOutputStream();
+            out.write(msg.toString().getBytes(charset));
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+}
---
+++ netty/src/main/java/com/max/netty/bio/client/BioClient.java
@@ -0,0 +1,12 @@
+package com.max.netty.bio.client;
+
+/**
+ * @author Max.Tu
+ * @program learning
+ * @description
+ * @create 2022-09-15 20:01
+ **/
+public class BioClient {
+
+
+}
